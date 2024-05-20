d490da825afeca4c25c8cf9b64d4d0a5d4adaf9c
sin <michaelrleo@gmail.com>
2024-05-20 20:41:05 +0000

    My second Terraform commit

--- data.tf
+++ data.tf
@@ -10,3 +10,7 @@ data "aws_ssm_parameter" "amzn2_linux" {
 #The label "aws_ssm_parameter" specifies the type of data source we're querying. In this case, an AWS Systems Manager (SSM) parameter.
 #The name label "amzn2_linux" is the unique identifer we've assigned the data source in order to easily reference the object elsewhere within the Terraform configuration. 
 #Within the block the key value "name" follwed by the string specifies the parameter to fetch from SSM. This parameter typically stores the AMI ID of the latest AMazon Linux 2 AMI, therefore it can be used to launch new EC2 instances with the latest Amazon Linux 2 image.
+
+data "aws_region" "currentregion" {
+  name = "us-east-1"
+}
